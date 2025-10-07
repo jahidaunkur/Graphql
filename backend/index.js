49bdb48a825e03527009d2f05bf22a54a12a44f7
MD. JAHIDUL ISLAM <79759048+JAHIDUL456@users.noreply.github.com>
2025-10-07 16:04:16 +0600

    learning graphql

---
+++ backend/index.js
@@ -0,0 +1,80 @@
+
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from "express-graphql";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList,
+} from "graphql";
+
+const users = []; 
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+    logoBase64: { type: GraphQLString }, 
+  },
+});
+
+
+const QueryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: () => users,
+    },
+  },
+});
+
+
+const MutationType = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+        logoBase64: { type: GraphQLString },
+      },
+      resolve: (_, args) => {
+        const id = Date.now().toString();   
+        const user = { id, ...args };
+        users.push(user);
+        return user;
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: QueryType,
+  mutation: MutationType,
+});
+
+const app = express();
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true, 
+  })
+);
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`GraphQL server running at http://localhost:${PORT}/graphql`);
+});
